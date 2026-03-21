beeef5a52bd359514b64580713483ba39f274351
gedoujou <gedoujou55@gmail.com>
2026-03-21 17:58:08 +0900

    update

--- index.js
+++ index.js
@@ -1,66 +1,19 @@
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-require('dotenv').config();
-const path = require('node:path');
-const fs = require("fs")
-const express = require('express');
 
-const client = new Client({
-intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.MessageContent, 
-  ],
+import { Client, GatewayIntentBits, Events } from "discord.js";
+import express from "express";
+var client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
-
-client.once('clientReady', () => {
-  console.log(`ログイン完了: ${client.user.tag}`);
-});
-
-client.commands = new Collection();
-
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`${filePath} に必要な "data" か "execute" がありません。`);
-	}
-}
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = interaction.client.commands.get(interaction.commandName);
-
-	if (!command) {
-		console.error(`${interaction.commandName} が見つかりません。`);
-		return;
-	}
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
-	}
-});
-
-client.login(process.env.token);
+client.login(process.env.TOKEN);
 
 client.on("clientReady", () => {
   console.log(`サーバーが起動しました!!`);
 });
-
-const app = express();
+var app = express();
 app.get("/", (req, res) => {
   res.send(`ok`);
 });
-
-const port = 3000;
+var port = 3000;
 app.listen(port, () => {
   console.log(`Good morning!!`);
 });
