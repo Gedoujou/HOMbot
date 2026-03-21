c6db8c6729180476adf0bd172b0899cf9629286a
gedoujou <gedoujou55@gmail.com>
2026-03-21 18:11:53 +0900

    update

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
-const { Client, GatewayIntentBits, Events } = require("discord.js");
-const express = require("express");
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+const path = require('node:path');
+const fs = require("fs");
+const express = require('express');
 
-var client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
 });
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command);
+  } else {
+    console.log(`${filePath} に必要な "data" か "execute" がありません。`);
+  }
+}
+
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = interaction.client.commands.get(interaction.commandName);
+  if (!command) {
+    console.error(`${interaction.commandName} が見つかりません。`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+  }
+});
+
+client.once('ready', () => {
+  console.log(`ログイン完了: ${client.user.tag}`);
+});
+
 client.login(process.env.TOKEN);
 
-client.on("clientReady", () => {
-  console.log(`サーバーが起動しました!!`);
-});
-
-var app = express();
+const app = express();
 app.get("/", (req, res) => {
   res.send(`ok`);
 });
 
-var port = 3000;
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log(`Good morning!!`);
 });
