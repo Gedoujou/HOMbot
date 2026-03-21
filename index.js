2a9c7484e2b8b0b8f33d2e12bbafc7c655be2f37
gedoujou <gedoujou55@gmail.com>
2026-03-21 18:01:12 +0900

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
+const { Client, GatewayIntentBits, Events } = require("discord.js");
+const express = require("express");
 
-import { Client, GatewayIntentBits, Events } from "discord.js";
-import express from "express";
 var client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
@@ -9,10 +9,12 @@ client.login(process.env.TOKEN);
 client.on("clientReady", () => {
   console.log(`サーバーが起動しました!!`);
 });
+
 var app = express();
 app.get("/", (req, res) => {
   res.send(`ok`);
 });
+
 var port = 3000;
 app.listen(port, () => {
   console.log(`Good morning!!`);
